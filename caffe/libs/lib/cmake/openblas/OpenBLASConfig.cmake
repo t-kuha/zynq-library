647e08004ccdaed81e93ca700d96124555a6c8d7
t-kuha <imagingtechnerd@gmail.com>
2018-04-17 10:46:57 +0000

    Revert DL framework files

--- caffe/libs/lib/cmake/openblas/OpenBLASConfig.cmake
+++
@@ -1,3 +0,0 @@
-SET(OpenBLAS_VERSION "0.2.20")
-SET(OpenBLAS_INCLUDE_DIRS /home/imagingtechnerd/caffe/caffe_lib/include)
-SET(OpenBLAS_LIBRARIES /home/imagingtechnerd/caffe/caffe_lib/lib/libopenblas.so)
