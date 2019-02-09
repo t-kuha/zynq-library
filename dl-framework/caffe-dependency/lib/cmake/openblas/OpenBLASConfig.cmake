dc6f993e6b5a00af878eb4175e75e79b0c94338d
t-kuha <imagingtechnerd@gmail.com>
2019-02-10 01:09:12 +0900

    Add OpenBLAS (v0.3.4)

---
+++ dl-framework/caffe-dependency/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.3.4")
+SET(OpenBLAS_INCLUDE_DIRS /home/pentaxmedical/tmp/OpenBLAS-0.3.4/_install/include)
+SET(OpenBLAS_LIBRARIES /home/pentaxmedical/tmp/OpenBLAS-0.3.4/_install/lib/libopenblas.so)
