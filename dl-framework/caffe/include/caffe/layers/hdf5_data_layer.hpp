a329537d28ed7f244222d3e7833646c44d2658c9
t-kuha <imagingtechnerd@gmail.com>
2019-05-12 17:52:42 +0900

    Update binaries & library

--- dl-framework/caffe/include/caffe/layers/hdf5_data_layer.hpp
+++ dl-framework/caffe/include/caffe/layers/hdf5_data_layer.hpp
@@ -1,3 +1,4 @@
+#ifdef USE_HDF5
 #ifndef CAFFE_HDF5_DATA_LAYER_HPP_
 #define CAFFE_HDF5_DATA_LAYER_HPP_
 
@@ -62,3 +63,4 @@ class HDF5DataLayer : public Layer<Dtype> {
 }  // namespace caffe
 
 #endif  // CAFFE_HDF5_DATA_LAYER_HPP_
+#endif // USE_HDF5
